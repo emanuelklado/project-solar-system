563e94b92c20914f41b944fad4e9220f35309482
emanuelklado <emanuelklado@msn.com>
2022-02-18 18:13:21 -0300

    requisito 9 ok

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,12 +1,23 @@
-// import React, { Component } from "react";
+import React, { Component } from 'react';
+import PropType from 'prop-types';
 
-// export default class MissionCard extends Component {
-//   render() {
-//     const { name, year, country, destination } = this.props;
-//     return (
-//       <div data-testid="mission-card">
-//         <p></p>
-//       </div>
-//     );
-//   }
-// }
+export default class MissionCard extends Component {
+  render() {
+    const { name, year, country, destination } = this.props;
+    return (
+      <div data-testid="mission-card">
+        <p data-testid="mission-name">{name}</p>
+        <p data-testid="mission-year">{year}</p>
+        <p data-testid="mission-country">{country}</p>
+        <p data-testid="mission-destination">{destination}</p>
+      </div>
+    );
+  }
+}
+
+MissionCard.propTypes = {
+  name: PropType.string.isRequired,
+  year: PropType.string.isRequired,
+  country: PropType.string.isRequired,
+  destination: PropType.string.isRequired,
+};
